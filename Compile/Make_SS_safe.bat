e88af5e387154420e18b9981c4dd873c0e36d6e9
RickMethot <richard.methot@noaa.gov>
2022-05-17 13:23:27 -0700

    Update Make_SS_safe.bat
    further clean-up of the windows make file

--- Compile/Make_SS_safe.bat
+++ Compile/Make_SS_safe.bat
@@ -1,20 +1,19 @@
-REM compiling ss.exe (safe executable) with generic path
-REM requires "Compile" directory in the same directory where
-REM the .tpl files and this .bat file sit.
+::compiling ss.exe (safe executable) with generic path
+::requires "Compile" directory in the same directory as the .tpl files and this .bat file
 cd ..
-REM deleted temporary file
+::deleted temporary file
 del SS_functions.temp
 
-REM create SS_functions.temp file combining various functions
+::create SS_functions.temp file combining various functions
 copy/b SS_biofxn.tpl+SS_miscfxn.tpl+SS_selex.tpl+SS_popdyn.tpl+SS_recruit.tpl+SS_benchfore.tpl+SS_expval.tpl+SS_objfunc.tpl+SS_write.tpl+SS_write_ssnew.tpl+SS_write_report.tpl+SS_ALK.tpl+SS_timevaryparm.tpl+SS_tagrecap.tpl SS_functions.temp
 
-REM combine remaining files to create ss.tpl
+::combine remaining files to create ss.tpl
 copy/b SS_versioninfo_330safe.tpl+SS_readstarter.tpl+SS_readdata_330.tpl+SS_readcontrol_330.tpl+SS_param.tpl+SS_prelim.tpl+SS_global.tpl+SS_proced.tpl+SS_functions.temp "Compile\ss.tpl"
 
-path=c:\admb;C:\rtools40\mingw64\bin;%path%
+::path=c:\admb;C:\rtools40\mingw64\bin;%path%
 
 cd "Compile"
-REM set CXX=cl
+::set CXX=cl
 set CXX=g++
 
 admb ss
